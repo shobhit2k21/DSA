e5e1f4e94d07581bc0b8dfb8bcab4d671c3f4f54
Shobhit Singh <shobhit2k21@gmail.com>
2026-04-28 14:23:09 +0530

    Add comments to clarify minOperations logic
    Refactor minOperations method to include comments explaining the logic for using median and remainder properties for operations.

--- LeetCode-Daily/Minimum Operations to Make a Uni-Value Grid.java
+++ LeetCode-Daily/Minimum Operations to Make a Uni-Value Grid.java
@@ -2,21 +2,21 @@ class Solution {
     public int minOperations(int[][] grid, int x) {
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int[] arr : grid)
-            for (int num : arr)
-                list.add(num);
+        for (int[] arr : grid)      // whenever we have to make elements equal always use median and remaider property
+            for (int num : arr)     // for median we use sorting in ascending order
+                list.add(num);      // after that we compare the remainder of all elements with the median when divided by x
 
         int n = list.size();
-        Collections.sort(list);
+        Collections.sort(list);  // sorting to get the median easily
 
-        int UniValue = list.get(n / 2);
+        int UniValue = list.get(n / 2);        // for minimum operation to make all elements equal always take the median value not the averge value
         int ans = 0;
 
         for (int num : list) {
-            if ((num % x) != (UniValue % x))
+            if ((num % x) != (UniValue % x))   // if the remainder of all elements are not equal to each other then it is impossible to make it equal by x 
                 return -1;
 
-            ans += Math.abs(num - UniValue) / x;
+            ans += Math.abs(num - UniValue) / x;  // count operation how much time we have to add or subtract x to make it equal the UniValue
         }
 
         return ans;
