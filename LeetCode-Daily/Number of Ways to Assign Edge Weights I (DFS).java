030a866c464e87e6ace66e2796ddbead731ad482
Shobhit Singh <shobhit2k21@gmail.com>
2026-06-11 13:23:56 +0530

    Implement DFS for edge weight assignment

---
+++ LeetCode-Daily/Number of Ways to Assign Edge Weights I (DFS).java
@@ -0,0 +1,49 @@
+class Solution {
+    int mod = (int) 1e9 + 7;
+
+    private int height(int node, List<List<Integer>> edge, boolean[] vis) {
+        int ans = 0;
+        for (int e : edge.get(node)) {
+            if (vis[e])
+                continue;
+
+            vis[e] = true;
+            ans = Math.max(1 + height(e, edge, vis), ans);
+        }
+
+        return ans;
+    }
+
+    private long power(long base, long exp) {
+        long ans = 1;
+
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                ans = (ans * base) % mod;
+            }
+
+            base = (base * base) % mod;
+            exp = exp >> 1;
+        }
+
+        return ans;
+    }
+
+    public int assignEdgeWeights(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> edge = new ArrayList<>();
+        for (int i = 0; i <= n + 1; i++)
+            edge.add(new ArrayList<>());
+
+        for (int a[] : edges) {
+            edge.get(a[0]).add(a[1]);
+            edge.get(a[1]).add(a[0]);
+        }
+
+        boolean[] vis = new boolean[n + 2];
+        vis[1] = true;
+
+        long h = height(1, edge, vis);
+        return (int) power(2, h - 1);
+    }
+}
